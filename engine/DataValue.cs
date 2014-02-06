a3db4c91c272c69f9897194aabafbdf5f47b826d
Wassili Hense <hense.wassili@googlemail.com>
2014-02-06 07:55:13 -0800

    engine/PLC BAK deserialize

--- engine/DataValue.cs
+++ engine/DataValue.cs
@@ -5,56 +5,56 @@ using System.Runtime.InteropServices;
 using System.Text;
 
 namespace X13 {
-  public struct DataValue {
-    public static implicit operator long(DataValue dv) {
-      switch((DataValueType)dv._type) {
-      case DataValueType.Null:
-        return 0L;
-      case DataValueType.Bool:
-      case DataValueType.Integer:
-      case DataValueType.DateTime:
-        return dv._ld.l;
-      case DataValueType.Float:
-        return (long)dv._ld.d;
-      default:
-        return (long)Convert.ToInt64(dv._o);
-      }
-    }
-    public static implicit operator DataValue(long v) {
-      var ret=new DataValue();
-      ret._type=(int)DataValueType.Bool;
-      ret._ld.l=v;
-      return ret;
-    }
-    public static implicit operator DataValue(bool b) {
-      var ret=new DataValue();
-      ret._type=(int)DataValueType.Bool;
-      ret._ld.l=b?1:0;
-      return ret;
-    }
-    private PriDT _ld;
-    private int _type;
-    private object _o;
-    private string _json;
-    public enum DataValueType : int {
-      Undefined = 0,
-      Null = 1,
-      Integer = 2,
-      DateTime = 3,
-      Bool = 4,
-      String = 5,
-      Float = 6,
-      Object = 7,
-      Array = 8,
-      Record = 9,
-      Binary = 10
-    }
-    [StructLayout(LayoutKind.Explicit)]
-    private struct PriDT {
-      [FieldOffset(0)]
-      public Int64 l;
-      [FieldOffset(0)]
-      public double d;
-    }
-  }
+  //public struct DataValue {
+  //  public static implicit operator long(DataValue dv) {
+  //    switch((DataValueType)dv._type) {
+  //    case DataValueType.Null:
+  //      return 0L;
+  //    case DataValueType.Bool:
+  //    case DataValueType.Integer:
+  //    case DataValueType.DateTime:
+  //      return dv._ld.l;
+  //    case DataValueType.Float:
+  //      return (long)dv._ld.d;
+  //    default:
+  //      return (long)Convert.ToInt64(dv._o);
+  //    }
+  //  }
+  //  public static implicit operator DataValue(long v) {
+  //    var ret=new DataValue();
+  //    ret._type=(int)DataValueType.Bool;
+  //    ret._ld.l=v;
+  //    return ret;
+  //  }
+  //  public static implicit operator DataValue(bool b) {
+  //    var ret=new DataValue();
+  //    ret._type=(int)DataValueType.Bool;
+  //    ret._ld.l=b?1:0;
+  //    return ret;
+  //  }
+  //  private PriDT _ld;
+  //  private int _type;
+  //  private object _o;
+  //  private string _json;
+  //  public enum DataValueType : int {
+  //    Undefined = 0,
+  //    Null = 1,
+  //    Integer = 2,
+  //    DateTime = 3,
+  //    Bool = 4,
+  //    String = 5,
+  //    Float = 6,
+  //    Object = 7,
+  //    Array = 8,
+  //    Record = 9,
+  //    Binary = 10
+  //  }
+  //  [StructLayout(LayoutKind.Explicit)]
+  //  private struct PriDT {
+  //    [FieldOffset(0)]
+  //    public Int64 l;
+  //    [FieldOffset(0)]
+  //    public double d;
+  //  }
+  //}
 }
--- engine/PLC/PLC.cs
+++ engine/PLC/PLC.cs
@@ -7,158 +7,136 @@ using System.Text;
 
 namespace X13.PLC {
   public class PLC {
-    public void Test() {
-      var engine = new ScriptEngine();
+    public ScriptEngine Engine;
 
-      //var obj=JSONObject.Parse(engine, "{ \"type\" : \"Statement\", \"declarer\" : \"Add\" }");
-      //engine.SetGlobalValue("console", new FirebugConsole(engine));
-      //engine.SetGlobalValue("root", new JuTopic(engine, Topic.root));
-      //var topics=engine.GetGlobalValue("topics");
-      //JSONObject.Parse
-      //JSONObject.Stringify
-
-      engine.SetGlobalValue("Decl", new DeclConstructor(engine));
-      string add=@"(function(){
-        var add=new Decl('Add');
-        add.Init=function(path){
-        }
-        add.Calc=function(sender){
-          var r=this.A+this.B;
-          if(this.C!=null){
-            r+=this.C;
+    public PLC() {
+      Engine = new ScriptEngine();
+    }
+    public void Parse(Topic dst, string json) {
+      object o=JSONObject.Parse(Engine, json);
+      ObjectInstance jo=o as ObjectInstance;
+      string jo_class;
+      if(jo!=null && (jo_class=jo.GetPropertyValue("class") as string)!=null) {
+        switch(jo_class) {
+        #region declarer
+        case "declarer": {
+            DeclInstance di=new DeclInstance();
+            di._name=dst.name;
+            foreach(var pr in jo.Properties) {
+              switch(pr.Name) {
+              case "Init": {
+                  string body=pr.Value as string;
+                  if(!string.IsNullOrEmpty(body)) {
+                    di.InitFunc=Engine.Function.Construct("path", body);
+                  }
+                }
+                break;
+              case "Calc": {
+                  string body=pr.Value as string;
+                  if(!string.IsNullOrEmpty(body)) {
+                    di.CalcFunc=Engine.Function.Construct("sender", body);
+                  }
+                }
+                break;
+              case "Deinit": {
+                  string body=pr.Value as string;
+                  if(!string.IsNullOrEmpty(body)) {
+                    di.DeinitFunc=Engine.Function.Construct(body);
+                  }
+                }
+                break;
+              case "class":
+                break;
+              default: {
+                  ObjectInstance p=pr.Value as ObjectInstance;
+                  if(p!=null && p.HasProperty("type")) {
+                    var pin =new DeclInstance.PinDecl();
+                    pin.name=pr.Name;
+                    if(p!=null) {
+                      foreach(var pi in p.Properties) {
+                        switch(pi.Name) {
+                        case "pos":
+                          pin.pos=Convert.ToInt32(pi.Value);
+                          break;
+                        case "type":
+                          pin.flags=(DeclInstance.Flags)Convert.ToInt32(pi.Value);
+                          break;
+                        }
+                      }
+                    }
+                    di._pins.Add(pin);
+                  }
+                }
+                break;
+              }
+            }
+            DeclInstance.functions[di._name]=di;
           }
-          this.Q=r;
+          break;
+        #endregion declarer
+        #region function
+        case "function": {
+            string jo_decl;
+            if((jo_decl=jo.GetPropertyValue("declarer") as string)!=null) {
+              DeclInstance decl;
+              if(DeclInstance.functions.TryGetValue(jo_decl, out decl)){
+                dst.value=new FuncInst(Engine, decl);
+                return;
+              } else {
+                Log.Warning("{0}[{1}] - unknown declarer", dst.path, jo_decl);
+              }
+            }
+          }
+          break;
+        #endregion function
         }
-        add.Register('A', Decl.ip, {'pos':1});
-        add.Register('B', Decl.ip);
-        add.Register('C', Decl.ip | Decl.optional);
-        add.Register('Q', Decl.op, {'pos':1});
-        return add;
-      })();";
+      }
+      dst.value=o;
+    }
+    public void Test() {
+      string add_json="{"
+        +"\"class\":\"declarer\","
+        +"\"Calc\":\"var r=this.A+this.B; if(this.C!=null){r+=this.C; } this.Q=r; \","
+        +"\"A\":{ \"type\":1, \"pos\":1},"
+        +"\"B\":{ \"type\":1, \"pos\":2},"
+        +"\"C\":{ \"type\":5, \"pos\":3},"
+        +"\"Q\":{ \"type\":2, \"pos\":1}"
+        +"}";
+      Parse(Topic.root.Get("/var/test"), "1");
+      Parse(Topic.root.Get("/etc/declarer/func/Add"), add_json);
+      var test=Topic.root.Get("/plc/test/A01");
+      string a01_json="{"
+        +"\"class\":\"function\","
+        +"\"declarer\":\"Add\""
+        +"}";
+      Parse(test, a01_json);
 
-      engine.Execute(add);
-      var test=Topic.root.Get("test");
-      test.value=DeclInstance.funcs[0].Init();
       Topic.Process();
       test.Get("A").value=1;
       test.Get("B").value=3;
       Topic.Process();
       test.Get("C").value=9;
       Topic.Process();
-      string json=JSONObject.Stringify(engine, test.value);
-      Log.Debug("{0}", json);
 
-      object o=JSONObject.Parse(engine, json);
-      ObjectInstance jo=o as ObjectInstance;
-      var test2=Topic.root.Get("test2");
-      if(jo!=null) {
-        string jo_decl;
-        if((jo.GetPropertyValue("class") as string)=="function" && (jo_decl=jo.GetPropertyValue("declarer") as string)!=null) {
-          var decl=DeclInstance.funcs.FirstOrDefault(z => z._name==jo_decl);
-          if(decl!=null) {
-            test2.value=decl.Init();
-          }
-        }
-      }
-      Topic.Process();
-      test2.Get("A").value=1;
-      test2.Get("B").value=3;
-      Topic.Process();
-      test2.Get("C").value=9;
-      Topic.Process();
-      json=JSONObject.Stringify(engine, test2.value);
+      string json=JSONObject.Stringify(Engine, Topic.root.Get("/etc/declarer/func/Add").value);
+      Log.Debug("{0}", json);
+      json=JSONObject.Stringify(Engine, test.value);
       Log.Debug("{0}", json);
 
     }
   }
-  //public class JuTopic : ObjectInstance {
-  //  private Topic _ref;
-  //  public JuTopic(ScriptEngine engine, Topic r)
-  //    : base(engine) {
-  //    PopulateFunctions();
-  //    _ref=r;
-  //  }
-  //  [JSProperty(Name = "name")]
-  //  public string Name {
-  //    get {
-  //      return _ref.name;
-  //    }
-  //  }
-  //  [JSProperty(Name = "value")]
-  //  public object value {
-  //    get {
-  //      return _ref.value;
-  //    }
-  //    set {
-  //      _ref.value=value;
-  //    }
-  //  }
-  //  [JSFunction(Name = "Get")]
-  //  public JuTopic Get(string name) {
-  //    return new JuTopic(base.Engine, _ref.Get(name, true));
-  //  }
-  //}
-
-  public class DeclConstructor : ClrFunction {
-    public DeclConstructor(ScriptEngine engine)
-      : base(engine.Function.InstancePrototype, "Func", new DeclInstance(engine.Object.InstancePrototype)) {
-      this.DefineProperty("ip", new PropertyDescriptor(1, PropertyAttributes.Sealed), true);
-      this.DefineProperty("op", new PropertyDescriptor(2, PropertyAttributes.Sealed), true);
-      this.DefineProperty("optional", new PropertyDescriptor(4, PropertyAttributes.Sealed), true);
-    }
-
-    [JSConstructorFunction]
-    public DeclInstance Construct(string name) {
-      return new DeclInstance(this.InstancePrototype, name);
-    }
-  }
-
-  public class DeclInstance : ObjectInstance {
-    public static List<DeclInstance> funcs=new List<DeclInstance>();
-    public string _name;
+  public class DeclInstance {
+    public static Dictionary<string, DeclInstance> functions=new Dictionary<string, DeclInstance>();
     public List<PinDecl> _pins;
+    public string _name;
 
-    public DeclInstance(ObjectInstance prototype)
-      : base(prototype) {
-      this.PopulateFunctions();
-    }
-
-    public DeclInstance(ObjectInstance prototype, string name)
-      : base(prototype) {
-      _name=name;
+    public DeclInstance() {
       _pins=new List<PinDecl>();
-      funcs.Add(this);
     }
 
-    [JSFunction(Name = "Register")]
-    public void Register(string variable, int flags) {
-      Register(variable, flags, null);
-    }
-    [JSFunction(Name = "Register")]
-    public void Register(string variable, int flags, ObjectInstance p) {
-      var pin =new PinDecl();
-      pin.name=variable;
-      pin.flags=(Flags)flags;
-      if(p!=null) {
-        foreach(var pi in p.Properties) {
-          switch(pi.Name) {
-          case "pos":
-            pin.pos=(int)pi.Value;
-            break;
-          }
-        }
-      }
-      _pins.Add(pin);
-    }
-    [JSProperty(Name = "Init")]
     public FunctionInstance InitFunc { get; set; }
-    [JSProperty(Name = "Calc")]
     public FunctionInstance CalcFunc { get; set; }
-    [JSProperty(Name = "Deinit")]
     public FunctionInstance DeinitFunc { get; set; }
-    public FuncInst Init() {
-      return new FuncInst(Engine, this);
-    }
     [Flags]
     public enum Flags {
       input=1,
@@ -188,20 +166,27 @@ namespace X13.PLC {
     private void SetOwner(Topic owner) {
       if(_owner!=owner) {
         if(_owner!=null) {
-
+          //TODO: something
         }
         _owner=owner;
         if(_owner!=null) {
           for(int i=_decl._pins.Count-1; i>=0; i--) {
-            base.DefineProperty(_decl._pins[i].name, new PropertyDescriptor(
-              new TopicGetter(Engine, this, _decl._pins[i]),
-              new TopicSetter(Engine, this, _decl._pins[i]),
-              PropertyAttributes.IsAccessorProperty | PropertyAttributes.NonEnumerable | PropertyAttributes.Writable), true);
+            var ts=new TopicSetter(Engine, this, _decl._pins[i]);
+            if((_decl._pins[i].flags & DeclInstance.Flags.input)==DeclInstance.Flags.input) {
+              base.DefineProperty(_decl._pins[i].name, new PropertyDescriptor(ts, null,
+                PropertyAttributes.IsAccessorProperty | PropertyAttributes.NonEnumerable), true);
+            } else {
+              base.DefineProperty(_decl._pins[i].name, new PropertyDescriptor(ts, ts,
+                PropertyAttributes.IsAccessorProperty | PropertyAttributes.NonEnumerable | PropertyAttributes.Writable), true);
+            }
           }
           if(_decl.InitFunc!=null) {
             _decl.InitFunc.Call(this, owner.path);
           }
           _owner.children.changed+=children_changed;
+          if(_decl.CalcFunc!=null) {
+            _decl.CalcFunc.Call(this, _owner.name);
+          }
         }
       }
     }
@@ -223,7 +208,7 @@ namespace X13.PLC {
         if((_decl.flags & DeclInstance.Flags.optional)!=DeclInstance.Flags.optional) {
           _ref=_owner._owner.Get(_decl.name);
         } else {
-          _owner._owner.Exist(_decl.name, out _ref);       
+          _owner._owner.Exist(_decl.name, out _ref);
         }
 
       }
@@ -234,31 +219,12 @@ namespace X13.PLC {
           }
           _ref.value=argumentValues[0];
           Log.Debug("{0}={1}", _ref.path, argumentValues[0]);
+        } else if(argumentValues.Length==0) {
+          if(_ref!=null || _owner._owner.Exist(_decl.name, out _ref)) {
+            return _ref.value;
+          }
         }
-        return null;
-      }
-    }
-    private class TopicGetter : FunctionInstance {
-      private   FuncInst _owner;
-      private   DeclInstance.PinDecl _decl;
-      private   Topic _ref;
-
-      public TopicGetter(ScriptEngine engine, FuncInst funcInst, DeclInstance.PinDecl pinDecl)
-        : base(engine) {
-        _owner = funcInst;
-        _decl = pinDecl;
-
-        if((_decl.flags & DeclInstance.Flags.optional)!=DeclInstance.Flags.optional) {
-          _ref=_owner._owner.Get(_decl.name);
-        }
-      }
-      public override object CallLateBound(object thisObject, params object[] argumentValues) {
-        if(_ref==null && !_owner._owner.Exist(_decl.name, out _ref)) {
-          Log.Debug("get {0}/{1} - undefined", _owner._owner.path, _decl.name);
-          return Undefined.Value;
-        }
-        Log.Debug("{0}={1}", _ref.path, _ref.value);
-        return _ref.value;
+        return Undefined.Value;
       }
     }
   }
